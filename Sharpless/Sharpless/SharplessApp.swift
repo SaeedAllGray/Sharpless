ab82b5dd791a9979daac73c252cdedd195970431
SaeedAllGray <saeedag.imail@gmail.com>
2021-10-25 12:27:25 +0330

    Xcode Project was added.

---
+++ Sharpless/Sharpless/SharplessApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharplessApp.swift
+//  Sharpless
+//
+//  Created by saeed on 10/25/21.
+//
+
+import SwiftUI
+
+@main
+struct SharplessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

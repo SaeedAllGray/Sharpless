f3ad08ff559bbeb8b8622b495790c116a9606b7e
SaeedAllGray <saeedag.imail@gmail.com>
2021-11-20 22:15:13 +0330

    arduino connected to wifi successfully.

--- Nansie/Nansie.ino
+++ Nansie/Nansie.ino
@@ -1,9 +1,12 @@
 //Saeed & Media
 // 2021 - All rights resereved.
 
-#include <EEPROM.h>
+#include <ESP8266WiFi.h>
 
 const byte LED_PIN = LED_BUILTIN;
+
+const String WIFI_SSID = "The Cyan One"; 
+const String PASSWORD = "October2020";
 
 ADC_MODE(ADC_VCC);
 
@@ -18,9 +21,34 @@ bool batteryNeedsReplacing()
   return voltage < 3.7;
 }
 
+bool isConnectedToWiFi() 
+{
+  return WiFi.status() == WL_CONNECTED;
+}
+
+void connectingToWiFi()
+{
+  String message = "Connecting to " + WIFI_SSID + "! Please Wait.";
+  Serial.println(message);
+  int counter = 0;
+  while (!isConnectedToWiFi())
+  {
+    delay(1000);
+    Serial.print("!");
+    counter++;
+  }
+  message = "Connected after " + String(counter) + " seconds.";
+  Serial.print(message);
+}
+
+
+
+
 void setup() {
   Serial.begin(9600);
+  WiFi.begin(WIFI_SSID, PASSWORD);
+  connectingToWiFi();
   setPinModes();
 }
 
-void loop() {}
+void loop() { }

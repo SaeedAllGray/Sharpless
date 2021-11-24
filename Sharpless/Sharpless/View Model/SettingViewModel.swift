fa9af2a601c10f36219623c7bbd6aa637c3c371c
Media Davarkhah <Media.dvh@gmail.com>
2021-11-24 13:57:33 +0330

    Hey Name Added

--- Sharpless/Sharpless/View Model/SettingViewModel.swift
+++ Sharpless/Sharpless/View Model/SettingViewModel.swift
@@ -9,14 +9,19 @@ import SwiftUI
 
 final class SettingViewModel: ObservableObject {
     @Published var fontSize: Double = 20
+    @Published var name: String = "Tom"
     
     let defaults = UserDefaults.standard
     
     init() {
         fontSize = defaults.double(forKey: "font size")
+        name = defaults.string(forKey: "name") ?? "Tom"
     }
     func setFontSize() {
         defaults.set(fontSize, forKey: "font size")
+    }
+    func setName() {
+        defaults.set(name, forKey: "Name")
     }
     func saveToUserDefaults<T>(key: String, value: T) {
         

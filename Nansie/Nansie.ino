64d2ab03714ea064ae21b3f9fd97a5d3ade126d3
unknown <you@example.com>
2021-12-06 15:37:41 +0330

    added OTA

--- Nansie/Nansie.ino
+++ Nansie/Nansie.ino
@@ -2,6 +2,9 @@
 // 2021 - All rights resereved.
 
 #include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 
 const byte LED_PIN = LED_BUILTIN;
 
@@ -59,13 +62,77 @@ void performPattern(char* pattern)
      }
   }
 }
+void OTAOnStart() 
+{
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH)
+    {
+      type = "sketch";
+    } else 
+    {
+      type = "filesystem";
+    }
+     Serial.println("Start Updating " + type);
+  });
+}
 
+void OTAOnEnd()
+{
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+}
+void OTAOnProgress()
+{
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+}
+void OTAOnError()
+{
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+}
+
+void configureOTA()
+{
+  // Port defaults to 8266
+  ArduinoOTA.setPort(8266);
+
+  ArduinoOTA.setHostname("Nansie_ESP8266");
+  MD5(sharpless) = 4431293f510370624fa504d839f00d1f;
+  ArduinoOTA.setPasswordHash("4431293f510370624fa504d839f00d1f");
+
+  OTAOnStart();
+  OTAOnEnd();
+  OTAOnProgress();
+  OTAOnError();
+  
+  ArduinoOTA.begin();
+  
+}
 void setup() {
   Serial.begin(9600);
   WiFi.begin(WIFI_SSID, PASSWORD);
   connectingToWiFi();
   setPinModes();
   performPattern("sssls"); 
+  configureOTA();
 }
 
-void loop() { }
+void loop() { 
+   ArduinoOTA.handle();
+}
